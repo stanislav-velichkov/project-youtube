5390bda2946445a0660d1dd9c84a79f21bd6f3ff
IvoFC <face9895@gmail.com>
2017-04-20 13:41:05 +0300

    Login

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var html = require('html');
+var session = require('express-session');
 var mongodb = require('mongo');
 var monk = require('monk');
 var db = monk("mongodb://admin:admin@ds157439.mlab.com:57439/youtube-db");
 
 // var index = require('./routes/index');
 // var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
+app.use(function (req, resp, next) {
+    req.db = db;
+    next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +32,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: '1234'}));
 
 // app.use('/', index);
 // app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,20 @@
-var epxress = require('express');
+var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res, next) {
-    var username = req.body.user;
+    var username = req.body.username;
     var password = req.body.password;
-
     var db = req.db;
     var users = db.get('users');
     users.find({user: username})
     .then(function(data) {
         if(data.length > 0) {
             req.session.userId = data[0]._id;
-            console.log(data);
             res.redirect('/');
         } else {
             res.render('login', { message: 'Try again'});
         }
     })
 })
+
+module.exports = router;
