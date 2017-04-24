a9e1263121ab6162d8b8a8321d16ee319eb12b08
Stanislav Velichkov <stanislav.v.velichkov@gmail.com>
2017-04-24 19:14:41 +0300

    Video Upload and Profile Style Changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var html = require('html');
 var hbs = require('hbs');
 var session = require('express-session');
+var fileUpload = require('express-fileupload');
 var mongodb = require('mongo');
 var monk = require('monk');
 var db = monk("mongodb://admin:admin@ds157439.mlab.com:57439/youtube-db");
@@ -15,6 +16,7 @@ var login = require('./routes/login');
 var register = require('./routes/register');
 var currentSession = require('./routes/session');
 var logout = require('./routes/logout');
+var upload = require('./routes/upload');
 
 var app = express();
 app.use(function (req, resp, next) {
@@ -33,12 +35,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: '1234'}));
+app.use(fileUpload());
 
 
 app.use('/login', login);
 app.use('/register', register);
 app.use('/session', currentSession);
 app.use('/logout', logout);
+app.use('/upload', upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/upload.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+  let uploadVideo = req.files.uploadVideo;
+ 
+  // Use the mv() method to place the file somewhere on your server 
+  uploadVideo.mv('./uploads/' + req.files.uploadVideo.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 
+    res.send('File uploaded!');
+  });
+});
+
+module.exports = router;
