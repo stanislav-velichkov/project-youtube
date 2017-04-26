ab7ac78033741f6e141f6043dc57b3a6d1653081
IvoFC <face9895@gmail.com>
2017-04-26 14:38:48 +0300

    Video service

--- public/assets/javascripts/MVC/userService.js
+++ public/assets/javascripts/MVC/userService.js
@@ -2,5 +2,6 @@ function User(username = '', email = '', password = '') {
     this.username = username;
     this.email = email;
     this.password = password;
-    this.friends = [];
+    this.likeds = [];
+    this.watchLater = [];
 }
