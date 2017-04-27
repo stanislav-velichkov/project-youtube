2e0ce8f708c4ee88e76fe0bbc135f728ce2928e0
Stanislav Velichkov <stanislav.v.velichkov@gmail.com>
2017-04-28 00:19:52 +0300

    Generating Thumbnail on Upload

--- public/assets/javascripts/model/upload.js
+++ public/assets/javascripts/model/upload.js
@@ -13,6 +13,56 @@ $("#uploadForm").change(function () {
         $("#failure").html('File too big! (Max Size: 100MB)');
     } else {
         isValid = true;
+
+
+        //Generating a snapshot
+        
+        // step 1
+        var canvas_elem = $( '<canvas class=snapshot_generator></canvas>' ).appendTo(document.body)[0];
+        var $video = $( '<video muted class=snapshot_generator></video>' ).appendTo(document.body);
+        //-----1------
+
+        //step 2
+        var url = URL.createObjectURL($('#uploadVideo').prop('files')[0]);
+        console.log($('#uploadVideo').prop('files')[0]);
+
+        var step_2_events_fired = 0;
+        $video.one('loadedmetadata loadeddata suspend', function() {
+            if (++step_2_events_fired == 3) {
+
+                // step 3
+                $video.one('seeked', function() {
+                    
+                    // step 4
+                    canvas_elem.height = this.videoHeight;
+                    canvas_elem.width = this.videoWidth;
+                    canvas_elem.getContext('2d').drawImage(this, 0, 0);
+                    var snapshot = canvas_elem.toDataURL();
+                    
+                    //snapshot - the image (base64)
+                    console.log(snapshot);
+
+                    // // append snapshot to body
+                    // var thumb = document.createElement('img');
+                    // thumb.setAttribute('src', snapshot);
+                    // document.body.appendChild(thumb);
+                    
+
+                    // Remove elements as they are no longer needed
+                    $video.remove();
+                    $(canvas_elem).remove();
+                    //-----4------
+
+                }).prop('currentTime', 3);
+                //-----3------
+
+            }
+        }).prop('src', url);
+
+        //-----2------
+
+        
+
     }
 });
 
