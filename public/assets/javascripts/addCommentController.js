06bc2309f7d071b6a627ec3171a56155b12fb028
Stanislav Velichkov <stanislav.v.velichkov@gmail.com>
2017-05-03 12:04:57 +0300

    Empty Comments Fix

--- public/assets/javascripts/addCommentController.js
+++ public/assets/javascripts/addCommentController.js
@@ -3,21 +3,32 @@ var app = angular.module('mainApp');
 
 app.controller('addCommentController', function ($scope, $rootScope, $http, $location) {
 
-    $scope.addComment = function () {
-        var indata = {
-            comment: $('#newCommentArea').val(),
-            user: $rootScope.username,
-            date: (new Date()).toGMTString(),
-            videoId: $rootScope.currentVideo._id
-        };
-        $rootScope.currentVideo.comments.push(indata);
-        console.log(indata);
-        $http.post('/comment', $rootScope.currentVideo)
-            .then(function (response, status, headers, config) {
-                $('#newCommentArea').val('');
+    $('#newCommentArea').bind('input propertychange', function () {
 
-            }, function (response, status, headers, config) {
-                alert('Server Error');
-            })
+        $("#commentsErr").css('display', 'none');
+
+    });
+
+
+    $scope.addComment = function () {
+        if ($('#newCommentArea').val().trim().length > 0) {
+            var indata = {
+                comment: $('#newCommentArea').val(),
+                user: $rootScope.username,
+                date: (new Date()).toGMTString(),
+                videoId: $rootScope.currentVideo._id
+            };
+            $rootScope.currentVideo.comments.push(indata);
+            console.log(indata);
+            $http.post('/comment', $rootScope.currentVideo)
+                .then(function (response, status, headers, config) {
+                    $('#newCommentArea').val('');
+
+                }, function (response, status, headers, config) {
+                    alert('Server Error');
+                })
+        } else {
+            $('#commentsErr').css('display', 'block');
+        }
     }
 });
