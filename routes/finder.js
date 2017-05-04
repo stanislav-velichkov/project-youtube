34b8554ff5987c25fea7ca744ea5b318f550c207
IvoFC <face9895@gmail.com>
2017-05-04 14:00:32 +0300

    Fix'd problem with get all videos for search

--- routes/finder.js
+++ routes/finder.js
@@ -4,36 +4,12 @@ var router = express.Router();
 router.post('/', function (req, res) {
     var db = req.db;
     var videos = db.get('videos');
-    var searchingArray = req.body.word.split(' ');
-
-    videos.find({
-        // title: {
-        //     $text: {
-        //         $search: { $regex: req.body.word},
-        //         $caseSensitive: false,
-        //         $diacriticSensitive: true
-        //     }
-        // }
-    })
+    videos.find({title: {$regex: req.body.word, $options: 'gi'}})
         .then(function (data) {
-            for (var index = 0; index < searchingArray.length; index++) {
-                var wordReg = new RegExp(searchingArray[index], 'gi');
-                var result = [];
-                data.forEach(function (obj) {
-                    if (obj.title.match(wordReg) != null) {
-                        if (result.indexOf(obj) == -1) {
-                            result.push(obj);
-                        }
-                    }
-                });
-            }
-            console.log(data);
-            res.json(result);
+            res.json(data);
         }).catch(function (err) {
         res.json(500, err);
     });
-
-
 });
 
 module.exports = router;
